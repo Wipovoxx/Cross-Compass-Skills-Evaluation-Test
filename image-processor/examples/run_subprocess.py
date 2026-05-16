09f59c598911edb46e588ba42eea926cfcd23013
Wipovoxx <wipovox@gmail.com>
2026-05-16 12:02:20 +0200

    More modifications to the sub_process example
    Added logging to better visualise how the subprocess executes the callbacks

--- image-processor/examples/run_subprocess.py
+++ image-processor/examples/run_subprocess.py
@@ -8,16 +8,19 @@ from collections.abc import Callable
 from typing import cast
 
 import edifice as ed
+import logging
+logger = logging.getLogger("Edifice")
+logger.setLevel(logging.INFO)
 
 
 def my_subprocess(
-    callback: Callable[[int], None],
+    callback: Callable[[int, int], None],
 ) -> None:
-    callback(1)
-    #time.sleep(1)
-    callback(2)
-    #time.sleep(1)
-    callback(3)
+    callback(1, 1)
+    time.sleep(1)
+    callback(2, 2)
+    time.sleep(1)
+    callback(3, 3)
 
 @ed.component
 def Main(self):
@@ -26,14 +29,16 @@ def Main(self):
     execute, set_execute = ed.use_state(False)
 
 
-    def my_callback(result: int):
+    def my_callback(result: int, process_id: int):
         if execute:
+            logger.info(f"Received result: {result} from process_id: {process_id}")
+            logger.info(f"Current results before update: {results} in process_id: {process_id}")
             set_results(lambda r: r + result) # noqa: RUF005
             set_execute(False)
         else:
             return  
 
-    ed.use_async(lambda:ed.run_subprocess_with_callback(my_subprocess, my_callback), start)
+    ed.use_async(lambda:ed.run_subprocess_with_callback(my_subprocess, my_callback), start, 3)
 
     def on_start_click(event):
         start_setter(not start)
